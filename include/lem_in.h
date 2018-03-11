10403a4b321c46273166586137a325e997b14903
Razshal <fontenille.mathieu@gmail.com>
2018-03-11 17:36:59 +0100

    pathfind in progress

--- include/lem_in.h
+++ include/lem_in.h
@@ -6,29 +6,42 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/11 12:50:37 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/03/11 14:17:43 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/03/11 17:12:37 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../libft/includes/libft.h"
 
 #ifndef LEM_IN_H
 # define LEM_IN_H
 # define SUCCESS -2
 # define FAILURE -1
+# define START -10
+# define END -20
 # define ENDOFTAB -2147483648
 # define INTMAX 2147483647
 
 typedef struct	s_room_list
 {
-	int room;
-	int *links;
-	t_room_list *next;
-}				t_room_list
+	int					room;
+	int					roomtype;
+	int					*links;
+	struct s_room_list	*next;
+}				t_room_list;
 
-typdef struct	s_lem_list
+typedef struct	s_lem_list
 {
-	int lem;
-	int room;
-	t_lem_list *next;
-}				t_lem_list
+	int					lem;
+	int					room;
+	struct s_lem_list	*next;
+}				t_lem_list;
+
+typedef struct	s_recurse
+{
+	t_room_list	*room_list;
+	t_lem_list	*lem_list;
+	int			room;
+}				t_recurse;
+
 
 #endif
--- src/searching.c
+++ src/searching.c
@@ -6,27 +6,63 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/11 12:48:17 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/03/11 14:50:33 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/03/11 17:30:13 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/lem_in.h"
 
-int searching(int actual_room, int link, int moove_count, t_room_list *list)
+static int is_end_room(int room, t_room_list *list)
 {
-	int testres;
+	t_room_list *current;
+
+	current = list;
+	while (current)
+	{
+		if (current->room == room && current->roomtype == END)
+			return (1);
+	}
+	return (0);
+}
+
+static t_room_list *get_room(int room, t_room_list *rlist)
+{
+	t_room_list *current;
+
+	current = rlist;
+	while (current)
+	{
+		if (current->room == room)
+			return (current);
+	}
+	return (NULL);
+}
+
+static int get_linkedroom(int desired_room, int desired_link, t_recurse *infos)
+{
+	t_room_list *room;
+
+	return (get_room(desired_room, infos->room_list)->links[desired_link]);
+}
+
+int searching(int actual_room, int link, int moove_count, t_recurse *infos)
+{
+	int test;
 	int res;
 	int room;
 
 	res = INTMAX;
-	if (is_end_case(actual_room))
+	if (is_end_room(actual_room, infos->room_list))
 		return (SUCCESS);
-	while ((room = get_linkedroom(actual_room, link)) != ENDOFTAB)
+	while ((room = get_linkedroom(actual_room, link, infos)) != ENDOFTAB)
 	{
-		if ((testres = searching(get_linkedroom(actual_room, link))) < res)
+		if ((test = searching(get_linkedroom(actual_room, link, infos),
+				link, moove_count + 1, infos)) < res)
 		{
-			res = testres;
-			room = get_linkedroom(actual_room, link);
+			res = test;
+			room = get_linkedroom(actual_room, link, infos);
 		}
+		link++;
 	}
+	return (moove_count);
 }
