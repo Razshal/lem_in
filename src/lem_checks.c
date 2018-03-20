27af26ae9022c273f2a64df6e37f9c383a4ebcc9
Razshal <fontenille.mathieu@gmail.com>
2018-03-20 17:16:20 +0100

    minor perf improvement

--- src/lem_checks.c
+++ src/lem_checks.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 13:05:18 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/03/16 17:22:50 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/03/20 17:16:00 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,13 @@
 int	are_lems_in_this_room(char *room_to_check, t_recurse *infos)
 {
 	t_lem_list	*current_lem;
-	int			are_they;
 
 	current_lem = infos->lem_list;
-	are_they = 0;
 	while (current_lem)
 	{
 		if (!ft_strcmp(current_lem->room, room_to_check))
-			are_they = 1;
+			return (1);
 		current_lem = current_lem->next;
 	}
-	return (are_they);
+	return (0);
 }
